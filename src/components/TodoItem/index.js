d80a0f296c9f1029827ceae27b987a435bc4c675
naveen <naveenrajoju1938@gmail.com>
2024-07-06 16:10:25 +0000

    first commit

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -2,19 +2,19 @@ import './index.css'
 
 const TodoItem = props => {
   const {todoDetails, deleteTodo} = props
-  const {id,title} = todoDetails
+  const {id, title} = todoDetails
   const onDeleteTodo = () => {
     deleteTodo(id)
   }
 
   return (
-    <li> 
-    <div className="todo-item">
-      <p className="title">{title}</p>
-      <button type="button" className="delete-btn" onClick={onDeleteTodo}>
-        Delete
-      </button>
-    </div>
+    <li>
+      <div className="todo-item">
+        <p className="title">{title}</p>
+        <button type="button" className="delete-btn" onClick={onDeleteTodo}>
+          Delete
+        </button>
+      </div>
     </li>
   )
 }
